5fe06e765b2ea723bf505236e6dda104dae904db
Nathanel Titane <nathanel.titane@gmail.com>
2020-04-01 10:25:00 -0400

    Models update

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# update #
+
+scripts_list=(
+	models-sync.sh
+	models-pages.sh
+)
+
+for scripts in ${scripts_list[@]}
+do
+	bash "$script"
+done
+
+# upload changes
+
+git add *
+
+git commit -a -m 'Models update'
+
+git push
