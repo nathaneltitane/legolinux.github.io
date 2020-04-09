bf40504109fcb97c7e84129b53123c5c590f6a74
Nathanel Titane <nathanel.titane@gmail.com>
2020-04-09 16:27:23 -0400

    Models update

--- javascript/set-random-background.js
+++ javascript/set-random-background.js
@@ -3,4 +3,18 @@
 $(document).ready(function() {
 	var random_digit = Math.floor(Math.random() * 10);
 	$('.background').css('background-image', 'url("/background/background-0' + random_digit + random_digit + '.png")');
+});// set random background //
+
+.ready(function() {
+var count = 106;
+
+function pad(str, max) {
+str = str.toString();
+return str.length < max ? pad("0" + str, max) : str;
+}
+
+.css(
+'background-image',
+'url("/background/background-' + pad(Math.floor(Math.random() * count), 3) + .png ")'
+);
 });
