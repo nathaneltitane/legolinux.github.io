3e2825371d79cbb5f8cd3ebc63a9dafdd4b02f67
Nathanel Titane <nathanel.titane@gmail.com>
2020-04-09 16:50:16 -0400

    Models update

--- models-sync.sh
+++ models-sync.sh
@@ -91,7 +91,7 @@ do
 
     for background in $(echo $(find $local_models_directory -type f -iname flat.$extension))
     do
-        if [ $count <= "99" ]
+        if [ $count =< "99" ]
         then
             count_prefix="-00"
         fi
