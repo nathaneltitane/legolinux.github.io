f5910cd7462454e33ecbde7dc247be1209391392
Nathanel Titane <nathanel.titane@gmail.com>
2020-04-09 16:22:05 -0400

    Models update

--- models-sync.sh
+++ models-sync.sh
@@ -103,6 +103,24 @@ do
         ((count++))
     done
 
-    echo $count
+    # update random background generator with new file count
+
+    echo "
+		// set random background //
+
+		$(document).ready(function() {
+			var count = 150;
+
+			function pad(str, max) {
+				str = str.toString();
+				return str.length < max ? pad("0" + str, max) : str;
+			}
+
+			$('.background').css(
+				'background-image',
+				'url("/background/background-' + pad(Math.floor(Math.random() * count), 3) + .png ")'
+			);
+		});
+    " > ./javascript/set-random-background.js
 
 done
