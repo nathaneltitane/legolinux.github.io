e41eaf0701016eed7fabc59086a5ba3dcfdbf985
Nathanel Titane <nathanel.titane@gmail.com>
2020-04-09 17:51:05 -0400

    Models update

--- models-sync.sh
+++ models-sync.sh
@@ -98,11 +98,12 @@ do
 
 	a=1
 
-	for i in ../background/*.png
+	for background in ../background/*.png
 	do
-		new=$(printf "%03d.png" "$a") #04 pad to length of 4
+		new=$(printf "%03d.png" "$a")
 
-		mv -i -- "$i" ../background/"$new"
+		mv -background -- "$background" ../background/background-"$new"
+
 		let a=a+1
 	done
 
