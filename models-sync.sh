38b3eead0b907adf783636af7af1e5fe7a0f33de
Nathanel Titane <nathanel.titane@gmail.com>
2020-04-09 17:12:31 -0400

    Models update

--- models-sync.sh
+++ models-sync.sh
@@ -91,7 +91,7 @@ do
 
     for background in $(echo $(find $local_models_directory -type f -iname flat.$extension))
     do
-		digit=$(seq -f "%05g" ${count})
+		digit=$(seq -f "%05g" 0 1000)
         rsync -avr "$background" "../background/background-$digit.png"
 
         ((count++))
