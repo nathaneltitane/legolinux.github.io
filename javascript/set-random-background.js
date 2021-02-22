edfa932df48e278e19b924cb496f35daeee15ef3
nathaneltitane <nathanel.titane@gmail.com>
2021-02-22 08:59:12 -0500

    Models update

--- javascript/set-random-background.js
+++ javascript/set-random-background.js
@@ -1,15 +1,15 @@
-	// set random background //
+// set random background //
 
-	$(document).ready(function() {
-	var count = 118;
+$(document).ready(function() {
+var count = 118;
 
-	function pad(str, max) {
-		str = str.toString();
-		return str.length < max ? pad("0" + str, max) : str;
-	}
+function pad(str, max) {
+	str = str.toString();
+	return str.length < max ? pad("0" + str, max) : str;
+}
 
-	$('.background').css(
-		'background-image',
-		'url("/background/background-' + pad(Math.floor(Math.random() * count), 3) + '.png ")'
-	);
-	});
+$('.background').css(
+	'background-image',
+	'url("/background/background-' + pad(Math.floor(Math.random() * count), 3) + '.png ")'
+);
+});
