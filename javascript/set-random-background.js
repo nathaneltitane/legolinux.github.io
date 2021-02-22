59b7338fc4598cfe435adbcec61c2bc3a47d93d1
nathaneltitane <nathanel.titane@gmail.com>
2021-02-22 08:50:39 -0500

    Code & Features Update

--- javascript/set-random-background.js
+++ javascript/set-random-background.js
@@ -1,14 +1,14 @@
 	// set random background //
 
-	\$(document).ready(function() {
-	var count = ${count};
+	.ready(function() {
+	var count = 118;
 
 	function pad(str, max) {
 		str = str.toString();
 		return str.length < max ? pad("0" + str, max) : str;
 	}
 
-	\$('.background').css(
+	.css(
 		'background-image',
 		'url("/background/background-' + pad(Math.floor(Math.random() * count), 3) + '.png ")'
 	);
