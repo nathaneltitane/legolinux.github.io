cd1275e09b82456d285eaca9c4e1e34ec6d7e34f
nathaneltitane <nathanel.titane@gmail.com>
2021-02-22 08:51:22 -0500

    Models update

--- javascript/set-random-background.js
+++ javascript/set-random-background.js
@@ -1,6 +1,6 @@
 	// set random background //
 
-	.ready(function() {
+	$(document).ready(function() {
 	var count = 118;
 
 	function pad(str, max) {
@@ -8,7 +8,7 @@
 		return str.length < max ? pad("0" + str, max) : str;
 	}
 
-	.css(
+	$('.background').css(
 		'background-image',
 		'url("/background/background-' + pad(Math.floor(Math.random() * count), 3) + '.png ")'
 	);
